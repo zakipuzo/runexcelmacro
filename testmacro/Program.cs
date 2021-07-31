dfba6be656612418303bf2b74ac3a415bf2a3424
zakaria.a <aarabzakaria7@gmail.com>
2021-07-31 18:22:18 +0100

    first commit

---
+++ testmacro/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace testmacro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runMacro();
+        }
+
+        public static void runMacro()
+        {
+            Excel.Application xlApp = new Excel.Application();
+
+            Excel.Workbook xlWorkBook;
+
+            //~~> Start Excel and open the workbook.
+            xlWorkBook = xlApp.Workbooks.Open("file path");
+
+            //~~> Run the macros by supplying the necessary arguments
+            xlApp.Run("macro name");
+
+            //~~> Clean-up: Close the workbook
+            xlWorkBook.Close(false);
+
+            //~~> Quit the Excel Application
+            xlApp.Quit();
+            Console.WriteLine("Macro was executed succesfully");
+            //~~> Clean Up
+            releaseObject(xlApp);
+            releaseObject(xlWorkBook);
+        }
+
+        public static void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
